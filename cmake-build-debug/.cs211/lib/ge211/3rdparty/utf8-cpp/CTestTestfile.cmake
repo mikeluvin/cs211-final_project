c23523bedc0a8353b49d54703d47fddec2c319b4
agdelesseps <agdelesseps@gmail.com>
2020-05-25 17:10:00 -0400

    Getting rid of Anton's cmake-build-debug

--- cmake-build-debug/.cs211/lib/ge211/3rdparty/utf8-cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/agdelesseps/Google Drive/YEAR2 Q3/COMP_SCI 211/fp/.cs211/lib/ge211/3rdparty/utf8-cpp
-# Build directory: /Users/agdelesseps/Google Drive/YEAR2 Q3/COMP_SCI 211/fp/cmake-build-debug/.cs211/lib/ge211/3rdparty/utf8-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
